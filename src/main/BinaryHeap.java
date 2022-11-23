9ad157eae53e79a272ef881740bccb5bf9baeab8
UO258418 <UO258418@uniovi.es>
2022-11-23 19:54:45 +0100

    filterDown done(To be tested)

---
+++ src/main/BinaryHeap.java
@@ -0,0 +1,75 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryHeap<T extends Comparable<T>> {
+
+    List<T> heap;
+
+    public BinaryHeap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public BinaryHeap(T[] heap) {
+        this();
+        for(int i = 0; i < heap.length; i++)
+            add(heap[i]);
+    }
+
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return heap.toString();
+    }
+
+    public void add(T element) {
+        if(element == null)
+            throw new NullPointerException("Element to add cannot be empty");
+
+        heap.add(element);
+        filterUp(heap.indexOf(element));
+    }
+
+    private void filterUp(int pos) {
+        int parent = (pos - 1) / 2;
+        if(heap.get(parent).compareTo(heap.get(pos)) > 0) {
+            Collections.swap(heap, pos, parent);
+        }
+
+        if(parent != 0)
+            filterUp(parent);
+    }
+
+    private void filterDown(int pos) {
+        int lowestChildPos = getLowestChild(pos);
+
+        if(lowestChildPos != -1) {
+            if(heap.get(pos).compareTo(heap.get(lowestChildPos)) > 0) {
+                Collections.swap(heap, pos, lowestChildPos);
+            }
+
+            filterDown(lowestChildPos);
+        }
+    }
+
+    private int getLowestChild(int parent) {
+        int left = parent * 2 + 1;
+        int right = parent * 2 + 2;
+
+        if(right < heap.size()) {
+            return heap.get(left).compareTo(heap.get(right)) <= 0 ? left : right;
+        }
+
+        if(left < heap.size()) {
+            return left;
+        }
+
+        return -1;
+    }
+
+}
